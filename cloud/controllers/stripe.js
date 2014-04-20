2c71d4baaaad1271ccb7d7198a8c08084f57bb84
Craig Martin <craig.j.martin2@gmail.com>
2014-04-19 21:32:21 -0700

    some stripe setup

--- cloud/controllers/stripe.js
+++ cloud/controllers/stripe.js
@@ -1,6 +1,20 @@
 
+//Setup Stripe
+var Stripe = require('stripe');
+Stripe.initialize('sk_test_193nrUQjYb2foSDVGqpMIA11');
+
+
+
+
+
+
+
+
+
+
+
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  response.success("One Warm Coat team is the coolest");
 });
