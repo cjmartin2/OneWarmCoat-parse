c7fd32e1ec0801095e9f7009e3b9f80b456bb0c1
Craig Martin <craig.j.martin2@gmail.com>
2014-04-22 00:11:50 -0700

    Processing the amount entered by the user

--- cloud/controllers/stripe.js
+++ cloud/controllers/stripe.js
@@ -5,16 +5,16 @@ Stripe.initialize('sk_test_uhfZ9lhMa1Dxwvr2VFn0rdmt');
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-
+Parse.Cloud.define("stripe_charge_customer", function(request, response) {
+	var amount = request.params['amount'];
 
 	Stripe.Charges.create({
-	  amount: 100 * 10, // $10 expressed in cents
+	  amount: amount * 10, // $10 expressed in cents
 	  currency: "usd",
 	  card: request.params['token'] // the token id should be sent from the client
 	},{
 	  success: function(httpResponse) {
-	    response.success("Purchase made!");
+	    response.success("User donated $" + amount + "!");
 	  },
 	  error: function(httpResponse) {
 	    response.error("Uh oh, something went wrong");
