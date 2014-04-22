f27a2f161b580543ce903e074549700ea09c0b57
Craig Martin <craig.j.martin2@gmail.com>
2014-04-21 21:51:32 -0700

    Stripe dummy charge

--- cloud/controllers/stripe.js
+++ cloud/controllers/stripe.js
@@ -3,18 +3,23 @@
 var Stripe = require('stripe');
 Stripe.initialize('sk_test_uhfZ9lhMa1Dxwvr2VFn0rdmt');
 
-
-
-
-
-
-
-
-
-
-
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
-  response.success("One Warm Coat team is the coolest");
+
+
+	Stripe.Charges.create({
+	  amount: 100 * 10, // $10 expressed in cents
+	  currency: "usd",
+	  card: request.params['token'] // the token id should be sent from the client
+	},{
+	  success: function(httpResponse) {
+	    response.success("Purchase made!");
+	  },
+	  error: function(httpResponse) {
+	    response.error("Uh oh, something went wrong");
+	  }
+	});
+
+  // response.success("One Warm Coat team is the coolest");
 });
